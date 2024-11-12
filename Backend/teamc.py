b30015176652a6d49c701627007943f582dfb52d
Kate Ergo <KateErgo@acknowledge.nl>
2024-11-12 16:22:02 +0100

    nette weergave

--- Backend/teamc.py
+++ Backend/teamc.py
@@ -4,7 +4,7 @@ from json import loads, dumps
 def start():
     file_path = 'meat_consumption_worldwide.csv'  
     df = pd.read_csv(file_path)
-    df = df.head()
+    df = df.head(20)
     print(df)
     result = df.to_json(orient="records")
     parsed = loads(result)
